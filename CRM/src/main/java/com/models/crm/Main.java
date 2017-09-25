69005a17ae8cbbd330ecc35ce089eebf90c00e7f
Petar Mitrevski <mitrevski.p@gmail.com>
2017-09-25 13:38:08 +0200

    Main promeneto

--- CRM/src/main/java/com/models/crm/Main.java
+++ CRM/src/main/java/com/models/crm/Main.java
@@ -5,15 +5,26 @@
  */
 package com.models.crm;
 
+import java.lang.annotation.Annotation;
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.AnnotationConfiguration;
 
-/**
- *
- * @author Petar
- */
-public class Main {
+        public class Main {
 
     public static void main(String[] args) {
+        User user = new User();
+        user.setUserName("Petar");
+        user.setUserID(1);
+        
+        SessionFactory sessionFactory = new AnnotationConfiguration().configure().buildSessionFactory();
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        
+        session.save(user);
+        
+        session
+        
         
     }
 
