56f42cf60f4f285ffe4b450f45fde67ef7a65829
Petar Mitrevski <mitrevski.p@gmail.com>
2017-09-27 09:10:37 +0200

    1

--- CRM/src/main/java/com/users/crm/UserController.java
+++ CRM/src/main/java/com/users/crm/UserController.java
@@ -27,6 +27,12 @@ public class UserController {
     
     public static void main(String[] args) throws Exception {
         SpringApplication.run(UserController.class, args);
+        
+        
+        
+        
+        User user1 = new User();
+        user1.setEmail("mitrevski.p@gmail.com");
     }
     
 }
